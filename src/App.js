3ee0e2007fc5bb4fd6055cdd88e39d9083bf9ac0
Sparsh <sparsh2000gupta@gmail.com>
2024-01-15 23:03:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount]= useState(0);
+  const handleClick1=()=>{
+    setCount(count+1);
+  };
+  const handleClick2=()=>{
+    setCount(count-1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={handleClick1}>Increment</button>
+      <button onClick={handleClick2}>Decrement</button>
+
     </div>
   );
 }
